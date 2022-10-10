b973052003b02cef4e82d04cd6cd541531853c15
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2022-10-10 14:03:18 -0300

    tabela de artigos sem opção de edição

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -5,8 +5,12 @@ const router = express.Router();
 const Category = require("../categories/Category");
 const Article = require("./Article");
 
-router.get("/articles", (req, res) => {
-  res.send("Rota de artigos");
+router.get("/admin/articles", (req, res) => {
+  Article.findAll({
+    include: [{ model: Category }]
+  }).then((articles) => {
+    res.render("admin/articles/index", { articles });
+  });
 });
 
 router.get("/admin/articles/new", (req, res) => {
@@ -23,7 +27,27 @@ router.post("/articles/save", (req, res) => {
     slug: slugify(title),
     body: body,
     categoryId: category
+  }).then(() => {
+    res.redirect("/admin/articles");
   });
+});
+
+router.post("/articles/delete", (req, res) => {
+  const { id } = req.body;
+
+  if (id != undefined) {
+    if (!isNaN(id)) {
+      Article.destroy({
+        where: {
+          id: id
+        }
+      }).then(() => res.redirect("/admin/articles"));
+    } else {
+      res.redirect("/admin/articles");
+    }
+  } else {
+    res.redirect("/admin/articles");
+  }
 });
 
 module.exports = router;
