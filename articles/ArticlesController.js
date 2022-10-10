b1b1d74537b42119548b42435d589aab5fa11697
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2022-10-10 13:30:18 -0300

    Cadastro de Artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,9 +1,29 @@
 const express = require("express");
+const slugify = require("slugify");
 
 const router = express.Router();
+const Category = require("../categories/Category");
+const Article = require("./Article");
 
 router.get("/articles", (req, res) => {
   res.send("Rota de artigos");
+});
+
+router.get("/admin/articles/new", (req, res) => {
+  Category.findAll().then((categories) => {
+    res.render("admin/articles/new", { categories });
+  });
+});
+
+router.post("/articles/save", (req, res) => {
+  const { title, body, category } = req.body;
+
+  Article.create({
+    title: title,
+    slug: slugify(title),
+    body: body,
+    categoryId: category
+  });
 });
 
 module.exports = router;
